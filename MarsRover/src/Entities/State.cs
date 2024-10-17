2d1d3c166d7fc9e52248d9224a128a56e295c118
Ninotter <nino.didier@live.fr>
2024-10-17 12:13:50 +0200

    Added test, changed solution structure

--- MarsRover/src/Entities/State.cs
+++ MarsRover/src/Entities/State.cs
@@ -6,7 +6,29 @@
         
         public double Vertical { get; set; } = 0;
         
+<<<<<<< HEAD
         public double RotationAngle {get; set;} = 0;
+=======
+        private double rotationAngle = 0;
+        public double RotationAngle {
+            get { return rotationAngle; }
+            set
+            {
+                if (value >= 360)
+                {
+                    rotationAngle = value - 360;
+                }
+                else if (value < 0)
+                {
+                    rotationAngle = 360 + value;
+                }
+                else
+                {
+                    rotationAngle = value;
+                }
+            }
+        }
+>>>>>>> c5ec924 (Added test, changed solution structure)
         
         public State(double horizontal, double vertical, double rotationAngle)
         {
