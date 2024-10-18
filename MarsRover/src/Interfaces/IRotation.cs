22d323a68b81e84b2b4fb995ea51935885d2a7be
epourexisterpro <epourexisterpro@gmail.com>
2024-10-18 10:09:16 +0200

    Deletion : Interface inutile + renommage de IMovement et ICommand.

--- MarsRover/src/Interfaces/IRotation.cs
+++
@@ -1,9 +0,0 @@
-﻿using Mars_Rover.src.Enum;
-
-namespace Mars_Rover.src.Interfaces
-{
-    public interface IRotation
-    {
-        OrientationsEnum Rotate90Degrees(bool isRight, OrientationsEnum oldOrientation);
-    }
-}
