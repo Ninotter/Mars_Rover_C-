423e04be67908ec87c9d052353af37112552c6f4
epourexisterpro <epourexisterpro@gmail.com>
2024-11-05 12:16:27 +0100

    feat(Interpreter): Finalisation des TU
    - Ajout des TU pour vérifier si le Rover fonctionne bien à la réception de toutes les commandes.
    
    Refs: Rover

--- RoverTest/InterpreterTest.cs
+++ RoverTest/InterpreterTest.cs
@@ -13,7 +13,7 @@ public class InterpreterTest
     }
 
     [Test]
-    public void MakeRoverGoForwardFromDefaultNorthPosition()
+    public void MakeRoverGoForwardFromDefaultNorthPosition_OnInfinitPlanet()
     {
         Interpreter.CreateRover(new InfinitePlanet());
         Interpreter.Send(Interpreter.FORWARD);
@@ -21,10 +21,91 @@ public class InterpreterTest
     }
     
     [Test]
-    public void MakeRoverGoBackwardFromDefaultNorthPosition()
+    public void MakeRoverGoBackwardFromDefaultNorthPosition_OnTorroidalPlanet()
     {   
         Interpreter.CreateRover(new TorroidalPlanet(10, 10));
         Interpreter.Send(Interpreter.BACKWARD);
         Assert.That(Interpreter.GetRoverState().Vertical, Is.EqualTo(10));
+    }
+
+    [Test]
+    public void MakeRoverTurnRightToFaceEast_OnInfinitPlanet()
+    {
+        Interpreter.CreateRover(new InfinitePlanet());
+        Interpreter.Send(Interpreter.RIGHT);
+        Assert.That(Interpreter.GetRoverState().Orientation, Is.EqualTo(State.EAST));
+    }
+    
+    [Test]
+    public void MakeRoverGoForwardFromEastPosition_OnInfinitPlanet()
+    {
+        Interpreter.CreateRover(new TorroidalPlanet(10, 10));
+        Interpreter.Send(Interpreter.RIGHT);
+        Interpreter.Send(Interpreter.FORWARD);
+        Assert.That(Interpreter.GetRoverState().Horizontal, Is.EqualTo(1));
+    }
+    
+    [Test]
+    public void MakeRoverGoBackwardFromEastPosition_OnTorroidalPlanet()
+    {   
+        Interpreter.CreateRover(new TorroidalPlanet(10, 10));
+        Interpreter.Send(Interpreter.RIGHT);
+        Interpreter.Send(Interpreter.BACKWARD);
+        Assert.That(Interpreter.GetRoverState().Horizontal, Is.EqualTo(10));
+    }
+    
+    [Test]
+    public void MakeRoverTurnRightTwiceToFaceSouth_OnInfinitPlanet()
+    {
+        Interpreter.CreateRover(new InfinitePlanet());
+        Interpreter.Send(Interpreter.RIGHT);
+        Interpreter.Send(Interpreter.RIGHT);
+        Assert.That(Interpreter.GetRoverState().Orientation, Is.EqualTo(State.SOUTH));
+    }
+    
+    [Test]
+    public void MakeRoverGoForwardFromSouthPosition_OnInfinitPlanet()
+    {
+        Interpreter.CreateRover(new TorroidalPlanet(10, 10));
+        Interpreter.Send(Interpreter.RIGHT);
+        Interpreter.Send(Interpreter.RIGHT);
+        Interpreter.Send(Interpreter.FORWARD);
+        Assert.That(Interpreter.GetRoverState().Vertical, Is.EqualTo(10));
+    }
+    
+    [Test]
+    public void MakeRoverGoBackwardFromSouthPosition_OnTorroidalPlanet()
+    {   
+        Interpreter.CreateRover(new TorroidalPlanet(10, 10));
+        Interpreter.Send(Interpreter.RIGHT);
+        Interpreter.Send(Interpreter.RIGHT);
+        Interpreter.Send(Interpreter.BACKWARD);
+        Assert.That(Interpreter.GetRoverState().Vertical, Is.EqualTo(1));
+    }
+    
+    [Test]
+    public void MakeRoverTurnLeftToFaceWest_OnInfinitPlanet()
+    {
+        Interpreter.CreateRover(new InfinitePlanet());
+        Interpreter.Send(Interpreter.LEFT);
+        Assert.That(Interpreter.GetRoverState().Orientation, Is.EqualTo(State.WEST));
+    }
+    
+    [Test]
+    public void MakeRoverGoForwardFromWestPosition_OnInfinitPlanet()
+    {
+        Interpreter.CreateRover(new TorroidalPlanet(10, 10));
+        Interpreter.Send(Interpreter.LEFT);
+        Interpreter.Send(Interpreter.FORWARD);
+        Assert.That(Interpreter.GetRoverState().Horizontal, Is.EqualTo(10));
+    }
+    
+    [Test]
+    public void MakeRoverGoBackwardFromWestPosition_OnTorroidalPlanet()
+    {   
+        Interpreter.CreateRover(new TorroidalPlanet(10, 10));
+        Interpreter.Send(Interpreter.LEFT);
+        Interpreter.Send(Interpreter.BACKWARD);
+        Assert.That(Interpreter.GetRoverState().Horizontal, Is.EqualTo(1));
     }
 }
